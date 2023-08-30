bcc66bb5dbcf34c1a58a2bdfb32302827304512e
gangz <gangz2009@gmail.com>
2023-08-30 15:46:01 +0800

    adjust style of plan

--- miniprogram/components/plan/plan.ts
+++ miniprogram/components/plan/plan.ts
@@ -11,7 +11,15 @@ Component({
    * 组件的初始数据
    */
   data: {
+    showAddressSelector: false,
+    currentAddress: 0, //0 - departure 1 - arrival
     numberValue: 1,
+    departure:{"name": "虹桥火车站南出站",
+    "latitude":31.19346,
+    "longtitude": 121.32074},
+    arrival:{"name": "东方明珠",
+    "latitude":31.23958,
+    "longtitude": 121.499763},
     timeRanges: [
       { value: "09:00-10:00", display: "上午 09:00 - 10:00" },
       { value: "10:00-11:00", display: "上午 10:00 - 11:00" },
@@ -20,14 +28,24 @@ Component({
     ],
     selectedTimeRange: { value: "09:00-10:00", display: "上午 09:00 - 10:00" }
   },
-  
+
   /**
    * 组件的方法列表
    */
   methods: {
-    handleNumberChange(event:any){
-      this.setData({numberValue:event.detail.value})
-  }
+    handleNumberChange(event: any) {
+      this.setData({ numberValue: event.detail.value })
+    },
+    handleTimeChange(event:any){
+      console.log(event)
+      this.setData({
+        selectedTimeRange: this.data.timeRanges[event.detail.value]
+      })
+    },
+    showAddress(index:number){
+      this.data.currentAddress=index;
+      this.data.showAddressSelector=true;
+    }
 
   }
 })
