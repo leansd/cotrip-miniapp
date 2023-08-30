5d7100d39495ca876bf1fcb39b03d7fd19d290c9
gangz <gangz2009@gmail.com>
2023-08-30 19:45:04 +0800

    add to meet components

---
+++ miniprogram/components/to-meet/to-meet.ts
@@ -0,0 +1,25 @@
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+      meetTime: '8:30',
+      phoneNumber: '18812341234',
+      cotripSeats: 1,
+      meetAddress: '虹桥火车站出租车上车点（北）'
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
