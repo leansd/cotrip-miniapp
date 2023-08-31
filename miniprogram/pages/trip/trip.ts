830bad7733db38b43606160e2b0fa35de116f58b
gangz <gangz2009@gmail.com>
2023-08-31 08:37:18 +0800

    refactory: remove unused code

--- miniprogram/pages/trip/trip.ts
+++ miniprogram/pages/trip/trip.ts
@@ -13,20 +13,8 @@ Page({
         }
       }
     },
-    numberValue: 1,
-    timeRanges: [
-      { value: "09:00-10:00", display: "上午 09:00 - 10:00" },
-      { value: "10:00-11:00", display: "上午 10:00 - 11:00" },
-      { value: "11:00-12:00", display: "上午 11:00 - 12:00" },
-      { value: "12:00-13:00", display: "中午 12:00 - 13:00" }
-    ],
-    selectedTimeRange: { value: "09:00-10:00", display: "上午 09:00 - 10:00" }
   },
   
-  handleNumberChange(event:any){
-      this.setData({numberValue:event.detail.value})
-  },
-
   handleTripPlanCreated(event:any){
     this.setData({currentTripPlan: event.detail});
   },
@@ -34,6 +22,4 @@ Page({
 	onLoad: function () {
   
   },
-  
-  
 });
