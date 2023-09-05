5426a6ad030760d30f5943fb77de1f96aca486ef
gangz <gangz2009@gmail.com>
2023-09-05 11:25:09 +0800

    put websocket connection related with 'waitting' component

--- miniprogram/components/waitting/waitting.ts
+++ miniprogram/components/waitting/waitting.ts
@@ -1,25 +1,8 @@
-import WebSocketAdapter from  '../../utils/ws_adapter'
-const wsAdapter = new WebSocketAdapter('ws://localhost:8081/notification', { 'user-id': 'user-id-1' });
+import WebSocketAdapter from  '../../utils/ws_adapter';
 var Stomp = require('../../utils/lib/stomp.orig');
+
 Stomp.setInterval = function () { }
 Stomp.clearInterval = function () { }
-var client = Stomp.over(wsAdapter);
-
-client.connect('user', 'pass', function (sessionId) {
-    console.log('sessionId', sessionId)
-
-    client.subscribe('/user/queue/status', (message) => {
-      console.log('Received message from /user/queue/status:', message.body);
-    });
-
-    // Subscribe to /topic/greetings
-    client.subscribe('/topic/greetings', (message) => {
-      console.log('Received message from /topic/greetings:', message.body);
-    });
-
-    // Send a message to /app/hello
-    client.send("/app/hello", {}, JSON.stringify({ 'name': 'AAA' }));
-});
 
 Component({
   /**
@@ -41,14 +24,41 @@ Component({
    */
   methods: {
     initStomp() {
-      
+      const wsAdapter = new WebSocketAdapter('ws://localhost:8081/notification', { 'user-id': 'user-id-1' });
+      const client = Stomp.over(wsAdapter);
+
+      client.connect('user', 'pass', (sessionId) => {
+        console.log('sessionId', sessionId);
+
+        client.subscribe('/user/queue/status', (message) => {
+          console.log('Received message from /user/queue/status:', message.body);
+        });
+
+        // Subscribe to /topic/greetings
+        client.subscribe('/topic/greetings', (message) => {
+          console.log('Received message from /topic/greetings:', message.body);
+        });
+
+        // Send a message to /app/hello
+        client.send("/app/hello", {}, JSON.stringify({ 'name': 'AAA' }));
+      });
+
+      // Store the client in the component's data for later use in the detached lifecycle
+      this.setData({ stompClient: client });
+    },
+    closeStomp() {
+      const client = this.data.stompClient;
+      if (client && client.connected) {
+        client.disconnect();
+      }
     }
   },
   lifetimes: {
-  attached() {
-    this.initStomp();
-  },
-  detached() {
+    attached() {
+      this.initStomp();
+    },
+    detached() {
+      this.closeStomp();
+    }
   }
-}
 });
