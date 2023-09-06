6d2cc710acc561e576bc9d3676517fc2b1c0a62a
gangz <gangz2009@gmail.com>
2023-09-06 14:54:22 +0800

    fix bug of auth refresh token

--- miniprogram/auth/auth.ts
+++ miniprogram/auth/auth.ts
@@ -11,6 +11,7 @@ const userLogin = () => {
     }
   });
 }
+
 const getToken = (): string | undefined => {
   return wx.getStorageSync('userToken').access_token;
 }
@@ -34,21 +35,25 @@ const requestToken = (code: string) => {
   });
 }
 
-
 const refreshToken = () => {
-  const tokens = wx.getStorageSync('userToken');
-  if (!tokens) {
-    return;
-  }
-  let REFRESH_INTERVAL = tokens.expires_in * 900;
-  if (REFRESH_INTERVAL<4*60*1000) REFRESH_INTERVAL = 4*60*1000;
-  setInterval(() => {
-    console.log('start refresh token')
+  let REFRESH_INTERVAL = wx.getStorageSync('userToken').expires_in * 900;
+  if (REFRESH_INTERVAL < 4 * 60 * 1000) REFRESH_INTERVAL = 4 * 60 * 1000;
+
+  const maxFailedTime = 3; // 设置最大失败次数，例如3次
+  let failedTime = 0;
+
+  const refreshTokenInterval = setInterval(() => {
+    console.log('start refresh token');
+    const currentTokens = wx.getStorageSync('userToken'); // 从存储中重新获取tokens
+    if (!currentTokens) {
+      clearInterval(refreshTokenInterval); // 如果没有token，停止定时器
+      return;
+    }
     wx.request({
       url: refreshUrl,
       method: 'POST',
       data: {
-        refresh_token: tokens.refresh_token
+        refresh_token: currentTokens.refresh_token // 使用最新的refresh_token
       },
       success: (response) => {
         if (response.data) {
@@ -57,10 +62,16 @@ const refreshToken = () => {
       },
       fail: (error) => {
         console.error('Token refresh failed', error);
+        failedTime++;
+        if (failedTime >= maxFailedTime) {
+          clearInterval(refreshTokenInterval); // 停止定时器
+          console.log('Stopped token refresh due to maximum failed attempts');
+        }
       }
     });
   }, REFRESH_INTERVAL);
 }
+
 export default {
   userLogin,
   getToken,
