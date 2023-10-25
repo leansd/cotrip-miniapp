bc1861db4f7fb73ee514843722ceb0b293ee6894
gangz <gangz2009@gmail.com>
2023-10-25 11:33:46 +0800

    fix date util problem, and redesign mine page to tab

--- miniprogram/pages/mine/mine.ts
+++ miniprogram/pages/mine/mine.ts
@@ -3,21 +3,22 @@ import {tripPlanUrl, tripRequest} from '../../api/trip/trip'
 Page({
   data: {
     tripPlans: [
-      {
-        departureLocation: '起始地示例',
-        arrivalLocation: '目的地示例',
-        date: '日期示例',
-        status: '状态示例',
-      },
-      // ... 更多 tripPlans 数据
+      
     ],
     textDescription: '<b>说明：</b><br><br>本小程序是<b>精益软件设计</b>方法的演示程序，并非实际运行业务。<br>方法细节请参见《软件设计：从专业到卓越》', 
     imageSrc: '../../img/leanbook.png', 
+    currentTab: 'trips'  // 默认选中
   },
 
   // 生命周期回调—监听页面加载
   onLoad: function(options) {
     this.loadTripPlans()
+  },
+
+  switchTab(event) {
+    this.setData({
+      currentTab: event.currentTarget.dataset.tab
+    });
   },
 
   loadTripPlans: function(){
