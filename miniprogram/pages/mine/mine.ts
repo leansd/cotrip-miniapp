3d2117210e2308a7ba9a6cfa278cb651e34469d3
gangz <gangz2009@gmail.com>
2023-10-25 10:18:36 +0800

    complete mine page: with all plans and note about the purpose

--- miniprogram/pages/mine/mine.ts
+++ miniprogram/pages/mine/mine.ts
@@ -1,4 +1,5 @@
-import auth from '../../api/auth/auth';
+import {tripPlanUrl, tripRequest} from '../../api/trip/trip'
+
 Page({
   data: {
     tripPlans: [
@@ -10,16 +11,31 @@ Page({
       },
       // ... 更多 tripPlans 数据
     ],
-    imageSrc: 'path/to/your/image.jpg', // 请更换成你的图片路径
-    textDescription: '这里是一些文字说明', // 文本框的说明文字
+    textDescription: '<b>说明：</b><br><br>本小程序是<b>精益软件设计</b>方法的演示程序，并非实际运行业务。<br>方法细节请参见《软件设计：从专业到卓越》', 
+    imageSrc: '../../img/leanbook.png', 
   },
 
   // 生命周期回调—监听页面加载
   onLoad: function(options) {
-    // 页面创建时执行
-    // 可以在这里做一些初始化工作，比如从服务器获取 tripPlans 数组
+    this.loadTripPlans()
   },
 
+  loadTripPlans: function(){
+    const componentInstance = this;
+    tripRequest({
+      url: tripPlanUrl,
+      method: 'GET',
+      data: {},
+      success(response){
+        componentInstance.setData({
+          tripPlans: response.data
+        });
+      },
+      fail(err){
+        console.error('创建出行计划失败',err)
+      }
+    });
+  }
   // ... 其他生命周期函数或自定义方法
 });
 
