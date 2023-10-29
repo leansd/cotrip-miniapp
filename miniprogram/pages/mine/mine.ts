c59f695250d0a95d79e4b6fea78d5e933a1db88f
gangz <gangz2009@gmail.com>
2023-10-29 13:23:50 +0800

    update notes

--- miniprogram/pages/mine/mine.ts
+++ miniprogram/pages/mine/mine.ts
@@ -5,7 +5,18 @@ Page({
     tripPlans: [
       
     ],
-    textDescription: '<b>说明：</b><br><br>本小程序是精益软件设计方法的演示程序，并非实际运行业务。<br>方法细节请参见《软件设计：从专业到卓越》', 
+    textDescription: '<b>这是一个演示程序，并非实际运行业务。</b>\
+    <br>它是“共享出行”演示项目的微信小程序前端。 \
+    <br>“共享出行”演示项目面向一线开发者和架构师，体现了: \
+    <li>- 领域驱动设计的战术模式和战略模式</li> \
+    <li>- 精益软件设计框架，包括了： \
+    精益需求分析、由外而内、测试先行、演进式设计等实践。</li>\
+    <li> - 持续集成和云原生部署 </li> \
+    <hr>项目采用Affero GPL许可证 \
+    <br>精益软件设计请参阅《软件设计：从专业到卓越》\
+    <br>网站链接： \
+    <li> - https://gitee.com/leansd \
+    <li> - https://www.leansd.cn', 
     imageSrc: '../../img/leanbook.png', 
     currentTab: 'trips'  // 默认选中
   },
